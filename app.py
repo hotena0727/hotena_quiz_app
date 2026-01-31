5387d8a68a071843e4e86ea481f170e8e4589772
hotena0727 <kissitai0727@gmail.com>
2026-01-31 21:12:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit as st
 from supabase import create_client
 
-SUPABASE_URL = st.secrets["https://xxxx.supabase.co"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_ANON_KEY = st.secrets["SUPABASE_ANON_KEY"]
 sb = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
