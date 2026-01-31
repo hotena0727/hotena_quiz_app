df0ba29b1819ecb4b0fc6218d658be6608875d43
hotena0727 <kissitai0727@gmail.com>
2026-01-31 16:42:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -371,7 +371,7 @@ if st.session_state.submitted:
         .floating-naver-talk:active {
             text-decoration: none !important;
             color: inherit !important;
-    }
+        }
 
     .talk-btn {
         background: #03C75A;
